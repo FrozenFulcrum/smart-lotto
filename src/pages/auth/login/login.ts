f85a40b5664558a10c23df07b94f420a2f9a93e0
Larry Damian <Larry@czarina-mbp.gateway.mts.net>
2018-07-15 20:51:57 -0500

    re-structure

---
+++ src/pages/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular/umd';
+
+@IonicPage()
+@Component({
+    selector: 'login-page',
+    template: `
+        <h3>Login Page</h3>
+    `
+})
+export class LoginPage {
+}
